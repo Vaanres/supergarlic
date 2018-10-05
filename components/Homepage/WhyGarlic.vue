1aa6b4883125dcc4af34a89b7c9044f6240fe216
Vaanres <Vaanres@Vaanres-Mac.local>
2018-10-05 17:53:10 +0700

    update

--- components/Homepage/WhyGarlic.vue
+++ components/Homepage/WhyGarlic.vue
@@ -19,12 +19,31 @@
             </div>
 
           </div>
-          <div class="grid_g__1"><div>L1</div></div>
-          <div class="grid_g__2"><div>L2</div></div>
-          <div class="grid_g__3"><div>L3</div></div>
-          <div class="grid_g__4"><div>L4</div></div>
-          <div class="grid_g__5"><div>L5</div></div>
-          <div class="grid_g__6"><div>L6</div></div>
+          <div class="grid_g__1">
+            <div class="d-inline-block item-detail text-center">
+              <p class="item-detail__title">110</p>
+              <span class="item-detail__description">Calories</span>
+            </div>
+          </div>
+          <div class="grid_g__2">
+            <div class="d-inline-block item-detail text-center">
+              <p class="item-detail__title">110</p>
+              <span class="item-detail__description">Calories</span>
+            </div>
+          </div>
+          <div class="grid_g__3">
+            <div class="d-inline-block item-detail text-center">
+              <p class="item-detail__title">110</p>
+              <span class="item-detail__description">Calories</span>
+            </div>
+          </div>
+          <div class="grid_g__4">
+            <div class="d-inline-block item-detail text-center">
+              <p class="item-detail__title">110</p>
+              <span class="item-detail__description">Calories</span>
+            </div>
+          </div>
+          
         </div>
       </div>
     </div>
@@ -54,23 +73,25 @@ export default {
 @supports (display: grid) {
   .grid_g {
     display: grid;
-    grid-template-columns: 10rem 1fr 10rem;
+    grid-template-columns: 1fr 1fr 1fr;
     grid-template-rows: auto;
     grid-template-areas:
-      'g1 m g4'
-      'g2 m g5'
-      'g3 m g6';
+      'g1 m g3'
+      'g2 m g4';
 
     &__0,
     &__1,
     &__2,
     &__3,
-    &__4,
-    &__5,
-    &__6 {
+    &__4 {
       align-self: center;
-      justify-content: center;
+      justify-self: center;
     }
+
+    // &__1,
+    // &__2 {
+    //   justify-self: end;
+    // }
 
     &__0 {
       grid-area: m;
@@ -86,12 +107,6 @@ export default {
     }
     &__4 {
       grid-area: g4;
-    }
-    &__5 {
-      grid-area: g5;
-    }
-    &__6 {
-      grid-area: g6;
     }
   }
 }
@@ -125,13 +140,22 @@ export default {
       }
 
       img {
-        width: 70%;
         z-index: 1;
       }
     }
 
     @include media-breakpoint-down(xs) {
       margin-bottom: 5rem;
+    }
+  }
+
+  .item-detail {
+    color: $garlic-dark;
+
+    &__title {
+      font-size: 3rem;
+      font-weight: 300;
+      margin-bottom: 0px;
     }
   }
 }
